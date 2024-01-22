0e52339d20c6135095f6278ea375f486c8bbe6b7
STALIN ESPINOZA CALLE <sespinozac@unemi.edu.ec>
2024-01-22 18:27:07 -0500

    anim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
 import { NavbarComponent } from './pages/navbar/navbar.component';
